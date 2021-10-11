7c02941d88d2ff7bdf24044006cf002ccc39323b
Leonardo Oliveira <30002511@students.ual.pt>
2021-10-11 22:41:57 +0100

    Inicialização de controladores

---
+++ controladores/pratica01.controller.js
@@ -0,0 +1,51 @@
+// CREATE - cria um novo registo
+exports.create = (req, res) => {
+    console.log("Create");
+    if (!req.body) {
+      return res.status(400).send({
+        message: "O conteúdo não pode ser vazio!",
+      });
+    }
+    return res.send({
+      message: "Criou um novo registo!",
+    });
+  };
+  
+  // READ all - busca todos os registos
+  exports.findAll = (req, res) => {console.log("FindAll")}
+  
+  // READ one - busca o item com um certo id (chave primária)
+  exports.findOne = (req, res) => {};
+  
+  // UPDATE - atualiza o item com o id recebido
+  exports.update = (req, res) => {
+    console.log("Update");
+    if (!req.body) {
+      return res.status(400).send({
+        message: "O conteúdo não pode ser vazio!",
+      });
+    }
+    return res.send({
+      message: "Atualizou um registo!",
+    });
+  };
+  
+  // DELETE one - elimina o item com o id recebido
+  exports.delete = (req, res) => {
+    console.log("Delete");
+    if (!req.body) {
+      return res.status(400).send({
+        message: "O conteúdo não pode ser vazio!",
+      });
+    }
+    return res.send({
+      message: "Eliminou um registo!",
+    });
+  };
+  
+  // DELETE all - elimina todos os itens
+  exports.deleteAll = (req, res) => {};
+  
+  // READ with condition - encontra todos os itens com uma certa condição = true
+  exports.findAllMarked = (req, res) => {};
+  
